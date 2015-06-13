3499950c52fd7068c5d79df9fe33ccd17d7c8658
Daniel de Mello Porto Steinnagel <daniel@steinnagel.se>
2015-06-13 22:24:36 +0200

    no message

--- server/server.js
+++ server/server.js
@@ -472,18 +472,20 @@ Meteor.startup(function () {
                     if (handleError(err)) return;
                     if (result.rows.length === 1) {
                         user.username = result.rows[0].name;
+                        user.profile.name = result.rows[0].name;
                         console.log("Validation successful. Setting username to: "+ user.username);
                     } else {
                         console.log("Validation unsuccessful.. Id="+user.services.google.id+" Result:"+JSON.stringify(result));
                     }
                     done();
+                    return user;
                 });
             });
         } else {
             console.log("Not a valid user..?");
+            return user;
         }
         console.log("Returning user: "+JSON.stringify(user));
-        return user;
     });
     console.log("Loading it up");
     // Clear state
